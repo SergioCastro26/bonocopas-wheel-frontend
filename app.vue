358123867815701c9267b43890e1c82f5cebb5c4
SergioCastro26 <sergiolcb2001@gmail.com>
2025-08-24 12:27:39 -0500

    Fix Vercel deployment - Static generation mode

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="dark">
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt'
@@ -14,5 +14,6 @@ export default defineNuxtConfig({
   build: {
     transpile: ['vue3-roulette']
   },
-  ssr: false
+  ssr: false,
+  target: 'static'
 })
